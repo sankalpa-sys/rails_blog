5efc471df13ec5a29b2c669a7f01a26005ff0927
Sankalpa Neupane <sankalpaneupane@Sankalpas-MacBook-Pro.local>
2021-08-13 16:28:06 +0545

    added friendly_to the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
-  validates_presence_of :title
-  validates_presence_of :body
+  validates :title, :presence => true, :length => {
+    :maximum => 50}
+    validates :body, :presence => true, :length => {
+      :minimum => 300}
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_rich_text :body
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_015658) do
+ActiveRecord::Schema.define(version: 2021_08_13_103307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,12 +56,25 @@ ActiveRecord::Schema.define(version: 2021_08_13_015658) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.integer "views", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
