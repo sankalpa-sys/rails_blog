835a7a0d95d618fb603c6e8a36e2644ab2015341
Sankalpa Neupane <sankalpaneupane@Sankalpas-MacBook-Pro.local>
2021-08-14 10:19:25 +0545

    Added off canvas

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :title, :presence => true, :length => {
     :maximum => 50}
     validates :body, :presence => true, :length => {
-      :minimum => 300}
+      :minimum => 250}
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_rich_text :body
